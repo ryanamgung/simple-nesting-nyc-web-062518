e3ae27de39d99afe81a6cbcd4ee50a63fa8723ee
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-19 03:37:53 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -130,6 +130,6 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 
 end
