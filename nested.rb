221425b28e1de9ae8eba727e16d715299bfac464
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-19 03:24:08 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages[0]]
+     programmer_hash[:dennis_ritchie][:languages.to_
 end
 
 def adding_matz
